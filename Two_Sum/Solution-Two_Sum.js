af14c7b0d8f828c5354fcc4cd9b3c61609f88853
Jan Kornienko <kor.jan@outlook.com>
2021-06-23 17:08:07 +0200

    Two Sum

---
+++ Two_Sum/Solution-Two_Sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (var i = 0; i < nums.length; i++) {
+        for (var u = 0; u < nums.length; u++) {
+            if (i != u) {
+                if ((nums[i] + nums[u]) == target) {
+                    return [i, u]
+                }
+            }
+        }
+    }
+};
